7327da48fd99fd9e1cf090e1a2a6586fcae8a090
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:40:16 +0530

    creating a state

--- client-1/src/Components/Posts/posts.js
+++ client-1/src/Components/Posts/posts.js
@@ -1,3 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Nav from '../Navigation/nav';
+
+class Posts extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            data:[
+                {
+                  commentToUser: ''
+                }
+              ],
+        }
+    }
+
+
+   
+    }
+}
+
+export default Posts;
