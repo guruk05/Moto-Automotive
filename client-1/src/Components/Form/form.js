da2c54a172d52ad3702de1972544367d3a1f0033
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:16:43 +0530

    imported react socket and bootstrap components

---
+++ client-1/src/Components/Form/form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Form, InputGroup , CardDeck , Button} from 'react-bootstrap';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from '../Navigation/nav';
+import '../../App.css';
+import openSocket from 'socket.io-client';
+const  socket = openSocket('http://localhost:8000');
+
+
+
+
