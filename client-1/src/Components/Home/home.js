cdd048519aa564d5892376b645693c08353f4776
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:53:22 +0530

    Removed a Dead Code

--- client-1/src/Components/Home/home.js
+++ client-1/src/Components/Home/home.js
@@ -6,9 +6,7 @@ import EmployeeForm from '../Form/form';
 // import Posts from './Posts.js';
 // import About from './About.js';
 // import Nav from './Nav';
-// import Authentication from './Authentication'
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Card from './Card.js'
+
 
 
 import openSocket from 'socket.io-client';
