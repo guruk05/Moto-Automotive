040619437bbf31c21b4f19fa34b680464f6c22dd
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:30:28 +0530

    imported react and nav components

---
+++ client-1/src/Components/About/about.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from '../Navigation/nav';
+// import FormPage from '../login/login';
+import NavbarPage from '../login/login'
