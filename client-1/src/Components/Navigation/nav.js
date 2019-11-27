e8079b4b0fae9afdc14fa39a925940a833ec44d7
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:38:23 +0530

    created a class

--- client-1/src/Components/Navigation/nav.js
+++ client-1/src/Components/Navigation/nav.js
@@ -5,3 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
     MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
+
+
+    class Nav extends Component{
+        constructor(props){
+            super(props)
+        }
+    
+        
+    }
+    
+    export default Nav;
+    
