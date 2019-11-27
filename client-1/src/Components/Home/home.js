19dcd075166c5cddc027b6c3919b766c91871ee8
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:32:54 +0530

    imported react and form component

---
+++ client-1/src/Components/Home/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import EmployeeForm from '../Form/form';
+// import Posts from './Posts.js';
+// import About from './About.js';
+// import Nav from './Nav';
+// import Authentication from './Authentication'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Card from './Card.js'
+// import socketIOClient from "socket.io-client";
+// import openSocket from 'socket.io-client';
+// import { subscribeToTimer } from './test';
+// import {Card, Form, InputGroup , CardDeck , Button} from 'react-bootstrap';
