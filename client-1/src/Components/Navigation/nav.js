42702078091742755bff2a153b79d93579b5cd5f
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:38:49 +0530

    returning a  nav component

--- client-1/src/Components/Navigation/nav.js
+++ client-1/src/Components/Navigation/nav.js
@@ -12,7 +12,45 @@ import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNav
             super(props)
         }
     
-        
+        render() {
+            return(
+          <MDBNavbar style={{ height: "80px", padding: 50 }} color="default-color" dark expand="md">
+            <MDBNavbarBrand>
+              <strong className="white-text"><h3 style={{color:"black"}}>MoTo WorkSpace</h3></strong>
+            </MDBNavbarBrand>
+            <MDBNavbarToggler />
+            <MDBCollapse id="navbarCollapse3"navbar>
+              <MDBNavbarNav left>
+                <MDBNavItem active>
+                  <MDBNavLink to="/Posts"><h3>Posts</h3></MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/About"><h3>Support</h3></MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/About"><h3>About</h3></MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                </MDBNavItem>
+              </MDBNavbarNav>
+              <MDBNavbarNav right>
+                <MDBNavItem>
+                  <MDBNavLink className="waves-effect waves-light" to="#!">
+                    <MDBIcon fab icon="twitter" />
+                  </MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink className="waves-effect waves-light" to="#!">
+                    <MDBIcon fab icon="google-plus-g" />
+                  </MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                </MDBNavItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBNavbar>
+            )
+        }
     }
     
     export default Nav;
