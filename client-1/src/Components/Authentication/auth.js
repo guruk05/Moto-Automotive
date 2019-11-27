d3c5118b85eddfb90222d1077d60873e3a2bb0f6
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:22:13 +0530

    importing react and axios and mdb bootstrap

---
+++ client-1/src/Components/Authentication/auth.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import EmployeeForm from '../Form/form';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBInput } from 'mdbreact';
+import 'mdbreact/dist/css/mdb.css';
+import './auth.css';
