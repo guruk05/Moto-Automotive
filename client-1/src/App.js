24dc99efa90b5c876f1e40b2af1f6a45b2b759ce
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-27 23:43:33 +0530

    importing react and bootstrap components

---
+++ client-1/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import EmployeeForm from './Form.js';
+import Posts from './Components/Posts/posts';
+import About from './Components/About/about';
+import Home from './Components/Home/home';
+import Nav from './Nav';
+import EmployeeForm from './Components/Form/form'
+import Authentication from './Components/Authentication/auth'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Card from './Card.js'
+// import socketIOClient from "socket.io-client";
+// import openSocket from 'socket.io-client';
+// import { subscribeToTimer } from './test';
+// import {Card, Form, InputGroup , CardDeck , Button} from 'react-bootstrap';
