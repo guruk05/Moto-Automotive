8517ce76b26b8858b0dedff8fef8cdf1cdaa6571
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-28 23:50:12 +0530

    created a render function and returning a jsx

--- client-2/src/Components/Navigation/nav.js
+++ client-2/src/Components/Navigation/nav.js
@@ -26,3 +26,37 @@ class Nav extends Component{
             }
     });
     }
+
+    render(){
+        return(
+            <div>
+            
+<header class="header-fixed">
+
+<div class="header-limiter">
+
+    <h1><a href="#">Company<span>logo</span></a></h1>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="#" class="selected">Blog</a>
+        <a href="#">Pricing</a>
+        <a href="#">About</a>
+        <a href="#">Faq</a>
+        <a href="#">Contact</a>
+    </nav>
+
+</div>
+
+</header>
+
+
+<div class="header-fixed-placeholder"></div>
+</div>
+
+
+
+
+        )
+    }
+}
