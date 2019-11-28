d5b126f7491cb5a3a3512678d50038290019f09b
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-28 23:37:45 +0530

    Removed React function component

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -2,12 +2,7 @@
 
 function App() {
   
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        
           Learn React
         </a>
       </header>
