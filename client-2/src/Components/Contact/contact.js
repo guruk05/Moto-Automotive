f28726d23b1189190906e7e70206f1b62e906d23
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:36:54 +0530

    removed dead code

--- client-2/src/Components/Contact/contact.js
+++ client-2/src/Components/Contact/contact.js
@@ -1,4 +1,1 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
-import myImage from './5.png';
+
