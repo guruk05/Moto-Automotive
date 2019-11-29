641d18866f308b51e580e917423deb00d7d7b8c8
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:44:17 +0530

    exported a function

--- client-2/src/Components/Faq/faq.js
+++ client-2/src/Components/Faq/faq.js
@@ -17,3 +17,87 @@ const useStyles = makeStyles(theme => ({
       fontWeight: theme.typography.fontWeightRegular,
     },
   }));
+
+
+  export default function SimpleExpansionPanel() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>Is MoTo only for Travellers? </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              No, Moto is also used by companies that monitors their assets in realtime.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography className={classes.heading}>By following what timebase MoTo workspaces are updated?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Moto workspaces are updated every time when allocation of asset (Bike) or change in quantity of asset (bike) and 
+              also in closing or opening of workspaces.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}>Is MoTo workspaces are 24/7?</Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}></Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}>Expansion Panel</Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}>Expansion Panel</Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}>Expansion Panel</Typography>
+          </ExpansionPanelSummary>
+        </ExpansionPanel>
+      </div>
+    );
+  }
