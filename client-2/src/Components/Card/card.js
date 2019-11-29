2dd73c99ad1b65de81bdaf235623651d3992038a
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:34:05 +0530

    included jsx

--- client-2/src/Components/Card/card.js
+++ client-2/src/Components/Card/card.js
@@ -6,4 +6,18 @@ import myImage from './5.png';
 
 class DisplayCard extends Component{
 
+
+    render(){
+        return(
+            <Card className="bg-dark text-white">
+  <Card.Img src={myImage} alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title></Card.Title>
+    <Card.Text>
+    </Card.Text>
+    <Card.Text></Card.Text>
+  </Card.ImgOverlay>
+</Card>
+)
+        }
 }
