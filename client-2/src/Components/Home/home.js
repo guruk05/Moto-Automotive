f0e57a203be0c9e0893e70483d1aec73c539816b
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:45:33 +0530

    imported react , bootstrap and socket.io

---
+++ client-2/src/Components/Home/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './home.css';
+// import { subscribeToTimer } from './test';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import socketIOClient from "socket.io-client";
+import {Card, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
+// import openSocket from 'socket.io-client';
+// const  socket = openSocket('http://localhost:8000');
+import axios from 'axios';
+import Nav from '../Navigation/nav';
+import SimpleSlider from '../Slider/slider';
+import Jumbotrons from '../Jumbotron/jumbotron';
+import DisplayCard from '../Card/card';
+import About from '../About/about';
+import Pricing from '../Pricing/pricing';
+import SimpleExpansionPanel from '../Faq/faq';
+import Contact from '../Contact/contact'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
