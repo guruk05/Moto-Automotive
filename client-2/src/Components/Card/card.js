5bfd8a5ba441906b8e5b7d2fd46f349258c9a4b7
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:33:32 +0530

    created a class

--- client-2/src/Components/Card/card.js
+++ client-2/src/Components/Card/card.js
@@ -2,3 +2,8 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
 import myImage from './5.png';
+
+
+class DisplayCard extends Component{
+
+}
