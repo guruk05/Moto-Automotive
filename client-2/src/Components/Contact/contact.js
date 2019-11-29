f141af4afaa93d72bfa2e1e0f8fbf6b804132109
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:37:11 +0530

    imported react

--- client-2/src/Components/Contact/contact.js
+++ client-2/src/Components/Contact/contact.js
@@ -1,1 +1,5 @@
-
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Form, Jumbotron,Container, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
+import axios from 'axios';
+import './contact.css';
