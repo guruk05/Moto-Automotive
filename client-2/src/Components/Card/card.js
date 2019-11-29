b97b280e698d258eb1a198d5e7eaa5bcf8f953fa
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:33:07 +0530

    importing react and bootstrap

---
+++ client-2/src/Components/Card/card.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
+import myImage from './5.png';
