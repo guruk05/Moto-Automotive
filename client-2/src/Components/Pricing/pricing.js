d6608edb81508e63bbbfbeb0661823cb5ce621f9
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:56:44 +0530

    created a class

--- client-2/src/Components/Pricing/pricing.js
+++ client-2/src/Components/Pricing/pricing.js
@@ -2,3 +2,7 @@ import React, { Component } from 'react';
 import './pricing.css';
 import Nav from '../Navigation/nav'
 import {Card, Jumbotron, Container, CardGroup, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
+
+
+class Pricing extends Component {
+}
