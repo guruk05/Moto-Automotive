10e532446dc7de0967a3a098ccc6eb79e5a45387
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:53:18 +0530

    included a render method

--- client-2/src/Components/Jumbotron/jumbotron.js
+++ client-2/src/Components/Jumbotron/jumbotron.js
@@ -8,5 +8,20 @@ class Jumbotrons extends Component{
 
     constructor(props){
         super(props)
-        
+
+}
+
+
+
+render(){
+    return(
+        <Jumbotron fluid className="jumbotron">
+<Container>
+<h1>Moto Automotive</h1>
+<p className="jumbotronText">
+  KickStart your day with MoTo, travel with ease and comfort. Get Streamed with live automobile location, availability and fuel updates.  
+</p>
+</Container>
+</Jumbotron>
+  
 }
