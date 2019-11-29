694f289f3203c44228a7db0d1d9a58523490acb4
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:43:20 +0530

    imported react and material ui comp

---
+++ client-2/src/Components/Faq/faq.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
