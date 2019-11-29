38c8134e3e93bbdf8fe305ad35de0ac68cdb1f2f
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-29 23:56:26 +0530

    import react

---
+++ client-2/src/Components/Pricing/pricing.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import './pricing.css';
+import Nav from '../Navigation/nav'
+import {Card, Jumbotron, Container, CardGroup, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
