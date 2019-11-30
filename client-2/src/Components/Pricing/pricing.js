246fde14f9c29bc277a61ead695440904302b35b
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 13:00:17 +0530

    updated pricing com

--- client-2/src/Components/Pricing/pricing.js
+++ client-2/src/Components/Pricing/pricing.js
@@ -5,20 +5,28 @@ import {Card, Jumbotron, Container, CardGroup, Form, InputGroup , CardDeck , But
 
 
 class Pricing extends Component {
+
     render(){
         return (
+          <div>
            <Jumbotron fluid className="pricing">
   <Container>
-    <h1>Fluid jumbotrons</h1>
-    <p>
-      This is a modified jumbotron that occupies the entire horizontal space of
-      its parent.
-    </p>
+    <h1 className="title">Pricing</h1>
   </Container>
 </Jumbotron>
+<h5 className="bodyText">MoTo workSpace pricing differs with respective to the assets which are to be monitored and 
+  streamed live. 
+  To ensure ease of monitoring and the best possible customer experience, we mandate you to join our premium platinum membership via MoTo partners and avail all the feature updates at zero cost.
+  Become a premium member and have access to all MoTo features which are available in our platform.
+  <br/>
+  <br/>
+  For further queries contact 1800 00 9000
+</h5>
+</div>
         )
+        
+    }
 }
-}
+
 
 export default Pricing;
-
