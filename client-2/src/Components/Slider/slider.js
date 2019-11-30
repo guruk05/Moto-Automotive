aeabd45ff4baf881e8f097cfff26afad32e40e7c
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 13:02:10 +0530

    included router in app.js

---
+++ client-2/src/Components/Slider/slider.js
@@ -0,0 +1,70 @@
+import React, { Component }from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
+import './slider.css';
+import myImage from './MotoSlide3.png';
+import sliderImage from './Motoslide1.png';
+import sliderImage1 from './MotoJourney.png';
+
+
+class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      cssEase: "linear"
+    };
+    return (
+      <div className="slider">
+        <Slider {...settings}>
+          <div>
+          <Card style={{height:"15rem"}} className="bg-dark text-white">
+  <Card.Img src={myImage} alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title></Card.Title>
+    <Card.Text>
+     
+    </Card.Text>
+    <Card.Text></Card.Text>
+  </Card.ImgOverlay>
+</Card>
+          </div>
+          <Card style={{height:"15rem"}} className="bg-dark text-white">
+  <Card.Img src={sliderImage} alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title></Card.Title>
+    <Card.Text>
+     
+    </Card.Text>
+    <Card.Text></Card.Text>
+  </Card.ImgOverlay>
+</Card>
+         
+          <Card style={{height:"15rem"}} className="bg-dark text-white">
+  <Card.Img src={sliderImage1} alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title></Card.Title>
+    <Card.Text>
+    </Card.Text>
+    <Card.Text></Card.Text>
+  </Card.ImgOverlay>
+</Card>
+        </Slider>
+      </div>
+    );
+  }
+}
+
+  export default SimpleSlider;
+
+
+
+     
