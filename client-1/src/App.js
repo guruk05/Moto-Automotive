81508850ecc93c84398451276facd28d5a9ad0a8
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 16:08:39 +0530

    formatted the code

--- client-1/src/App.js
+++ client-1/src/App.js
@@ -1,54 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import Posts from './Components/Posts/posts';
-import About from './Components/About/about';
-import EmployeeForm from './Components/Form/form'
-import Authentication from './Components/Authentication/auth'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import "./App.css";
+import Posts from "./Components/Posts/posts";
+import About from "./Components/About/about";
+import EmployeeForm from "./Components/Form/form";
+import Authentication from "./Components/Authentication/auth";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
-
-	constructor(props) {
-	  super(props);
-	  this.state = {
-      data:"",
-      isLoggedIn:false
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "",
+      isLoggedIn: false
+    };
   }
-  
-    handleChange = () =>{
-      this.setState({
-        isLoggedIn: !this.state.isLoggedIn
-      },()=>{
-        localStorage.setItem("loggedIn",this.state.isLoggedIn)
-      })
-    }
-	  
-    showData = (e) => {
 
-    }
-    
-    receiveDataFromChild = (data) => {
-      socket.emit('send message', data);
-      this.setState({data});
-    }
-	
-	render() {
-    const { isLoggedIn } = this.state
-	  return (
+  handleChange = () => {
+    this.setState(
+      {
+        isLoggedIn: !this.state.isLoggedIn
+      },
+      () => {
+        localStorage.setItem("loggedIn", this.state.isLoggedIn);
+      }
+    );
+  };
+
+  showData = e => {};
+
+  receiveDataFromChild = data => {
+    socket.emit("send message", data);
+    this.setState({ data });
+  };
+
+  render() {
+    const { isLoggedIn } = this.state;
+    return (
       <div>
         <Router>
-            <Switch>  
-              <Route exact path = '/Posts' component = {Posts}/>
-              <Route exact path = '/About' component = {About}/>
-              <Route path = '/' render={()=>localStorage.getItem("loggedIn")?(<EmployeeForm/>):(<Authentication toggle={this.handleChange}/>)}/>
-            </Switch> 
+          <Switch>
+            <Route exact path="/Posts" component={Posts} />
+            <Route exact path="/About" component={About} />
+            <Route
+              path="/"
+              render={() =>
+                localStorage.getItem("loggedIn") ? (
+                  <EmployeeForm />
+                ) : (
+                  <Authentication toggle={this.handleChange} />
+                )
+              }
+            />
+          </Switch>
         </Router>
       </div>
-    )
-	}
+    );
+  }
 }
 
 export default App;
