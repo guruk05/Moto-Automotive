3fc01972d6a4982dac2cfe0238c9fd957ee95f8c
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 16:05:11 +0530

    formatted the code

--- client-2/src/Components/About/about.js
+++ client-2/src/Components/About/about.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Jumbotron, Container} from 'react-bootstrap';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Jumbotron, Container } from "react-bootstrap";
 
-import './about.css';
+import "./about.css";
 
-
-class About extends Component{
-
-    render(){
-        return(
-       <div>
-           <Jumbotron fluid className="pricing">
-  <Container>
-    <h1 className="title">About us</h1>
-  </Container>
-</Jumbotron>
-<h5 className="bodyText">
-If you want to travel, you can take cabs or scooters and travel on your own.
-But to avail a scooter or bike you have to walk to nearby 
-Automobile Station, if your nearest Automobile station doesn't have any bikes
-even if they have bikes will you know if the available bikes are in rideable condition
-and whether the bike has sufficient fuel required for your travel. 
-<p>
-  <br/>
-Ok lets not make that much complicated, you choose a random bike and kickstart to ride , 
-unfortunately displayed fuel point is not sufficient to travel ..... Dont worry ..... 
-Welcome to MoTo .....
-we keep you updated on Automobile stations 24/7 with Realtime datas and live updates. We value your time.</p>
-
-Make decisions without visiting stations and travel with ease and comfort.  
-
-
-  <br/>
-  <br/>
-</h5>
-</div>
-        )
-    }
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron fluid className="pricing">
+          <Container>
+            <h1 className="title">About us</h1>
+          </Container>
+        </Jumbotron>
+        <h5 className="bodyText">
+          If you want to travel, you can take cabs or scooters and travel on
+          your own. But to avail a scooter or bike you have to walk to nearby
+          Automobile Station, if your nearest Automobile station doesn't have
+          any bikes even if they have bikes will you know if the available bikes
+          are in rideable condition and whether the bike has sufficient fuel
+          required for your travel.
+          <p>
+            <br />
+            Ok lets not make that much complicated, you choose a random bike and
+            kickstart to ride , unfortunately displayed fuel point is not
+            sufficient to travel ..... Dont worry ..... Welcome to MoTo ..... we
+            keep you updated on Automobile stations 24/7 with Realtime datas and
+            live updates. We value your time.
+          </p>
+          Make decisions without visiting stations and travel with ease and
+          comfort.
+          <br />
+          <br />
+        </h5>
+      </div>
+    );
+  }
 }
 
 export default About;
