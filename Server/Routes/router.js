77d7d43766d9849d847e763253f5ee8a85ad3700
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 13:24:02 +0530

    updated router.js

---
+++ Server/Routes/router.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const EmployeeInputs = require('../Models/employeeInputs');   
+const EmployeeCredentials = require('../Models/userRegistration');
+// server
+
+
+
+router.get("/posts",  async(req,res) => {
+    try {
+        const responses = await EmployeeInputs.find();
+        // console.log(responses);
+        res.send(responses);
+        
+    }
+    catch (err) {
+        console.log(err);
+    }
+  });
+
+
+router.post("/login", async(req,res) => {
+    try {
+        const responses = await EmployeeCredentials.find();
+        const requestUser = req.body.auth;
+        const dbUser = responses[0].credentials;
+        if (requestUser.username == dbUser.employeeId && 
+            requestUser.password == dbUser.employeePassword) {
+                console.log("Found")
+                res.sendStatus(200);
+            }
+         else {
+             res.sendStatus(404);
+         }
+    }
+    catch (err) {
+        res.sendStatus(404);
+    }
+})
+
+
+
+
+  module.exports = router;
