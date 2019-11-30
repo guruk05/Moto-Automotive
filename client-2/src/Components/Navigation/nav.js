82aec0b7dbf530263b8a7fdbcd7f2e92a6ec3d60
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 12:55:08 +0530

    updated nav component

--- client-2/src/Components/Navigation/nav.js
+++ client-2/src/Components/Navigation/nav.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './nav.css';
 import $ from 'jquery';
-
 
 class Nav extends Component{
     constructor(props){
@@ -13,8 +13,10 @@ class Nav extends Component{
             var showHeaderAt = 150;
             var win = $(window),
                     body = $('body');
-            
+            // Show the fixed header only on larger screen devices
             if(win.width() > 600){
+                // When we scroll more than 150px down, we set the
+                // "fixed" class on the body element.
                 win.on('scroll', function(e){
                     if(win.scrollTop() > showHeaderAt) {
                         body.addClass('fixed');
@@ -27,23 +29,49 @@ class Nav extends Component{
     });
     }
 
+
+
     render(){
         return(
+
             <div>
             
 <header class="header-fixed">
 
 <div class="header-limiter">
 
-    <h1><a href="#">Company<span>logo</span></a></h1>
+    <h1><a href="#"><span className="compName1">&nbsp;M</span>
+    <span className="compName2">&nbsp;o</span>
+    <span className="compName3">&nbsp;T</span>
+    <span className="compName4">&nbsp;o</span>
+    </a></h1>
 
     <nav>
-        <a href="#">Home</a>
-        <a href="#" class="selected">Blog</a>
-        <a href="#">Pricing</a>
-        <a href="#">About</a>
-        <a href="#">Faq</a>
-        <a href="#">Contact</a>
+        
+        <Link to="/">
+        <a>Home</a>
+        </Link>
+
+        {/* <Link>
+        <a>Blog</a>
+        </Link> */}
+
+        <Link to="/pricing">
+        <a>Pricing</a>
+        </Link>
+
+        <Link to="/about">
+        <a>About</a>
+        </Link>
+
+        <Link to="/faq">
+        <a>Faq</a>
+        </Link>
+
+        <Link to="/contact">
+        <a>Contact</a>
+        </Link>
+
     </nav>
 
 </div>
@@ -53,13 +81,12 @@ class Nav extends Component{
 
 <div class="header-fixed-placeholder"></div>
 </div>
-
-
-
-
+  
         )
     }
 }
 
-
 export default Nav;
+
+
+
