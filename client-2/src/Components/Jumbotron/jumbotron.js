a4356234abdd6742215111d4972d5121f07a94fe
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 12:54:01 +0530

    updated jumbotron comp

--- client-2/src/Components/Jumbotron/jumbotron.js
+++ client-2/src/Components/Jumbotron/jumbotron.js
@@ -5,25 +5,23 @@ import './style.css'
 
 
 class Jumbotrons extends Component{
-
     constructor(props){
         super(props)
+    }
 
-}
 
-render(){
-    return(
-        <Jumbotron fluid className="jumbotron">
-<Container>
-<h1>Moto Automotive</h1>
-<p className="jumbotronText">
-  KickStart your day with MoTo, travel with ease and comfort. Get Streamed with live automobile location, availability and fuel updates.  
-</p>
-</Container>
+    render(){
+        return(
+            <Jumbotron fluid className="jumbotron">
+  <Container>
+    <h1>Moto Automotive</h1>
+    <p className="jumbotronText">
+      KickStart your day with MoTo, travel with ease and comfort. Get Streamed with live automobile location, availability and fuel updates.  
+    </p>
+  </Container>
 </Jumbotron>
-    )
-}
+        )
+    }
 }
 
 export default Jumbotrons;
-
