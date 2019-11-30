3ef4d3bb2d2680cb23604613c60a89b788657b8e
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-30 12:51:50 +0530

    updated faq component

--- client-2/src/Components/Faq/faq.js
+++ client-2/src/Components/Faq/faq.js
@@ -5,99 +5,134 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
+import {Card, Jumbotron, Container, CardGroup, Form, InputGroup , CardDeck , Button, Toast} from 'react-bootstrap';
 
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
+  root: {
+    width: '100%',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    // padding: '10em',
+    marginLeft:'25em'
+    // height:'50em'
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(23),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+export default function SimpleExpansionPanel() {
+  const classes = useStyles();
+
+  return (
+    <div>
+    <div>
+           <Jumbotron fluid className="pricing">
+  <Container>
+    <h1 className="title">FAQ</h1>
+  </Container>
+</Jumbotron>
+<h5 className="bodyText">
+  <br/>
+  <br/>
+</h5>
+</div>
+<div className={classes.root} style={{width:"50rem"}}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Is MoTo only for Travellers?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          No, Moto is also used by companies that monitors their assets in realtime.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>By following what timebase MoTo workspaces are updated?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          Moto workspaces are updated every time when allocation of asset (Bike) or change in quantity of asset (bike) and 
+            also in closing or opening of workspaces.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography className={classes.heading}>Is MoTo workspaces are 24/7?</Typography>
+          </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          yes MoTo workspaces are 24/7 !
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography className={classes.heading}>What are MoTo Workspaces?</Typography>
+          </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          MoTo WorkSpaces are virtual automobile stations streamed to users with live updates.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography className={classes.heading}>Is MoTo only bound to Automobile?</Typography>
+          </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          Absolutely no!. We are providing individual workspaces for companies that requires 
+          tracking of their assets.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography className={classes.heading}>Feature Updates of MoTo?</Typography>
+          </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          Automating WorkSpaces and running it on zero man power.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      
+    </div>
+</div>
+  );
+}
 
 
-  export default function SimpleExpansionPanel() {
-    const classes = useStyles();
-  
-    return (
-      <div className={classes.root}>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className={classes.heading}>Is MoTo only for Travellers? </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              No, Moto is also used by companies that monitors their assets in realtime.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
-          >
-            <Typography className={classes.heading}>By following what timebase MoTo workspaces are updated?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Moto workspaces are updated every time when allocation of asset (Bike) or change in quantity of asset (bike) and 
-              also in closing or opening of workspaces.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography className={classes.heading}>Is MoTo workspaces are 24/7?</Typography>
-          </ExpansionPanelSummary>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography className={classes.heading}></Typography>
-          </ExpansionPanelSummary>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography className={classes.heading}>Expansion Panel</Typography>
-          </ExpansionPanelSummary>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography className={classes.heading}>Expansion Panel</Typography>
-          </ExpansionPanelSummary>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3a-content"
-            id="panel3a-header"
-          >
-            <Typography className={classes.heading}>Expansion Panel</Typography>
-          </ExpansionPanelSummary>
-        </ExpansionPanel>
-      </div>
-    );
-  }
+
+
